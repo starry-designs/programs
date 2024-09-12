7352022b8b4d8ceea05da5ac0cd0f43b8fccc31d
starry-designs <ungusoalex@gmail.com>
2024-09-12 15:24:34 +0300

    C programs

---
+++ Area.c
@@ -0,0 +1,17 @@
+// program to find the area of a rectangle
+#include <stdio.h>
+
+int main() 
+{    
+    int a, b, area;
+    printf("enter the first number a");
+    scanf("%d",&a);
+    
+    printf("enter the second number b");
+    scanf("%d",&b);
+    
+    area=a*b;
+    printf("%d",area);
+
+  
+}
---
+++ sum.c
@@ -0,0 +1,17 @@
+// program to add 2 numbers
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, sum;
+    printf("enter the first number a:");
+    scanf("%d",&a);
+    
+    printf("enter the second number b:");
+    scanf("%d",&b);
+    
+    sum=a+b;
+    printf("%d",sum);
+    
+    return 0;
+}
